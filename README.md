a76f8825faa07969e7865125aadcf5e0a419edd9
Antonio Camargo <antoniop.camargo@gmail.com>
2019-05-15 00:05:41 -0300

    Add a CLI interface and installation script

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # RNAsamba
-Coding score calculation using deep learning
+
+A tool for computing the coding potential of RNA transcript sequences using deep learning classification model
+
+## Overview
+
+
+## Installation
+
+```
+# Using pip
+$ pip install rnasamba
+
+# using conda
+$ conda install -c bioconda rnasamba
+```
+
+## Download the pre-trained model
+
+We provide a HDF5 file containing the weights of a model trained with human data. This model achieves high classification performance even in distant species (see reference). You can download the file by executing the following command:
+
+```
+$ curl -O https://raw.githubusercontent.com/apcamargo/RNAsamba/master/data/weights_master_model.hdf5
+```
+
+In case you want to train your model, you can follow the steps described in the section below.
+
+## Usage
+
+
+
+## Reference
+> Camargo, Antonio P., Vsevolod Sourkov, and Marcelo F. Carazzolle. "RNAsamba: coding potential assessment using ORF and whole transcript sequence information." *BioRxiv* (2019).
--- rnasamba/core/model.py
+++ rnasamba/core/model.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+#   This file is part of the rnasamba package, available at:
+#   https://github.com/apcamargo/RNAsamba
+#
+#   Rnasamba is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#   Contact: antoniop.camargo@gmail.com
+
 import logging
 
 import numpy as np
@@ -9,8 +29,8 @@ from keras.layers import (Activation, Concatenate, Dense, Dropout, Embedding,
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 
-from rnasamba.inputs import RNAsambaInput
-from rnasamba.miniigloo import IGLOO1D, RNAsambaAttention
+from rnasamba.core.inputs import RNAsambaInput
+from rnasamba.core.miniigloo import IGLOO1D, RNAsambaAttention
 
 
 class RNAsambaClassificationModel:
